98a67087a03e03f542bde8359d171f2dea021c75
Kushagra Pandey <kpandeyce008@gmail.com>
2021-08-10 10:25:40 +0530

    Updated samplling to clip reconstructions

--- models/diffusion/ddpm.py
+++ models/diffusion/ddpm.py
@@ -29,16 +29,28 @@ class DDPM(pl.LightningModule):
         self.betas = torch.from_numpy(betas)
         self.alphas = torch.from_numpy(alphas)
         self.alpha_bar = torch.from_numpy(alpha_bar)
+        self.alpha_bar_shifted = torch.from_numpy(alpha_bar_shifted)
         self.sigma_t = torch.from_numpy(sigma_t)
 
         self.const_dev_set = False
 
-    def compute_noisy_input(self, x, eps, t):
-        # Samples the noisy input x_t ~ N(x_t|x_0) in the forward process
-        return (
-            x * torch.sqrt(self.alpha_bar[t])[:, None, None, None]
-            + eps * torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None]
-        )
+    def get_posterior_mean_covariance(self, x_t, t, clip_denoised=True):
+        t_ = torch.full((x_t.size(0),), t, device=x_t.device, dtype=torch.long)
+        # Generate the reconstruction from x_t
+        x_recons = (
+            x_t - self.decoder(x_t, t_) * torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None]
+        ) / torch.sqrt(self.alpha_bar[t])[:, None, None, None]
+
+        # Clip
+        if clip_denoised:
+            x_recons.clamp_(-1.0, 1.0)
+
+        # Compute posterior mean from the reconstruction
+        a_1 = torch.sqrt(self.alpha_bar_shifted[t]) * self.betas[t] / (1 - self.alpha_bar[t])
+        a_2 = torch.sqrt(self.alphas[t]) * (1 - self.alpha_bar_shifted[t]) / (1 - self.alpha_bar[t])
+        post_mean = a_1[:, None, None, None] * x_recons + a_2[:, None, None, None] * x_t
+        post_variance = self.sigma_t[t][:, None, None, None]
+        return post_mean, post_variance
 
     def forward(self, x_t):
         # The sampling process goes here!
@@ -51,15 +63,18 @@ class DDPM(pl.LightningModule):
         self.alpha_bar.to(dev)
         self.sigma_t.to(dev)
 
-        for t in tqdm(range(self.T - 1, -1, -1)):
+        for t in tqdm(reversed(range(0, self.T))):
             z = torch.randn_like(x_t)
-            pred_mean = (
-                x
-                - (self.betas[t] / (torch.sqrt(1 - self.alpha_bar[t])))
-                * self.decoder(x_t, torch.tensor([t], device=dev))
-            ) / torch.sqrt(self.alphas[t])
-            x = pred_mean + torch.sqrt(self.sigma_t[t]) * z
+            post_mean, post_variance = self.get_posterior_mean_covariance(x, t)
+            x = post_mean + torch.sqrt(post_variance) * z
         return x
+
+    def compute_noisy_input(self, x, eps, t):
+        # Samples the noisy input x_t ~ N(x_t|x_0) in the forward process
+        return (
+            x * torch.sqrt(self.alpha_bar[t])[:, None, None, None]
+            + eps * torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None]
+        )
 
     def training_step(self, batch, batch_idx):
         x = batch
@@ -73,10 +88,10 @@ class DDPM(pl.LightningModule):
             self.const_dev_set = True
 
         # Sample timepoints
-        t = torch.randint(0, self.T, size=(x.size(0),)).to(dev)
+        t = torch.randint(0, self.T, size=(x.size(0),), device=dev)
 
         # Sample noise
-        eps = torch.randn_like(x).to(dev)
+        eps = torch.randn_like(x)
 
         # Predict noise
         x_t = self.compute_noisy_input(x, eps, t)
