57c6feb1022e4984af267ba0b73b7da1b8566330
Kushagra Pandey <kpandeyce008@gmail.com>
2021-08-14 11:41:38 +0530

    Updated ddpm training script

--- train_ddpm.py
+++ train_ddpm.py
@@ -6,25 +6,35 @@ import torchvision.transforms as T
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
+from torchvision.transforms.transforms import RandomHorizontalFlip
 
 from models.diffusion.ddpm import DDPM
-from models.diffusion.unet import UNet
+from models.diffusion.unet import Unet
 from util import configure_device, get_dataset
 
 
 logger = logging.getLogger(__name__)
 
 
+def __parse_str(s):
+    split = s.split(",")
+    return [int(s) for s in split]
+
+
 @click.command()
 @click.argument("root")
-@click.option("--t-embed", default=64)
+@click.option("--dim", default=64)
+@click.option("--attn-resolutions", default="0,0,0,1")
+@click.option("--n-residual", default=1)
+@click.option("--dim-mults", default="1,2,4,8")
+@click.option("--dropout", default=0)
+@click.option("--n-heads", default=1)
 @click.option("--beta1", default=1e-4, type=float)
 @click.option("--beta2", default=0.02, type=float)
-@click.option("--n-steps", default=1000)
+@click.option("--n-timesteps", default=1000)
 @click.option("--fp16", default=False)
 @click.option("--batch-size", default=32)
 @click.option("--epochs", default=1000)
-@click.option("--workers", default=4)
 @click.option("--log-step", default=1)
 @click.option("--device", default="gpu:0")
 @click.option("--chkpt-interval", default=1)
@@ -34,7 +44,9 @@ logger = logging.getLogger(__name__)
 @click.option("--restore-path", default=None)
 @click.option("--results-dir", default=os.getcwd())
 @click.option("--dataset", default="celeba-hq")
+@click.option("--flip", default=False)
 @click.option("--image-size", default=128)
+@click.option("--workers", default=4)
 @click.option("--subsample-size", default=None)  # Integrate this!
 def train(root, **kwargs):
     # Transforms
@@ -43,6 +55,7 @@ def train(root, **kwargs):
     transforms = T.Compose(
         [
             T.Resize(image_size),
+            T.RandomHorizontalFlip() if kwargs.get("flip") else T.Lambda(lambda t: t),
             T.CenterCrop(image_size),
             T.ToTensor(),
             T.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
@@ -56,20 +69,20 @@ def train(root, **kwargs):
     batch_size = kwargs.get("batch_size")
     batch_size = min(N, batch_size)
 
-    # Loader
-    loader = DataLoader(
-        dataset,
-        batch_size,
-        num_workers=kwargs.get("workers"),
-        pin_memory=True,
-        shuffle=True,
-        drop_last=True,
-        persistent_workers=True,
-    )
-
     # Model
+    # TODO: EMA is not yet setup. In the original paper they have used EMA!
     lr = kwargs.get("lr")
-    decoder = UNet(t_embed_dim=kwargs.get("t_embed"))
+    attn_resolutions = __parse_str(kwargs.get("attn-resolutions"))
+    dim_mults = __parse_str(kwargs.get("dim_mults"))
+    assert len(attn_resolutions) == len(dim_mults)
+    decoder = Unet(
+        dim=kwargs.get("dim"),
+        attn_resolutions=attn_resolutions,
+        n_residual_blocks=kwargs.get("n_residual"),
+        dim_mults=dim_mults,
+        dropout=kwargs.get("dropout"),
+        n_heads=kwargs.get("n_heads"),
+    )
     ddpm = DDPM(
         decoder,
         beta_1=kwargs.get("beta1"),
@@ -88,8 +101,8 @@ def train(root, **kwargs):
     results_dir = kwargs.get("results_dir")
     chkpt_callback = ModelCheckpoint(
         dirpath=os.path.join(results_dir, "checkpoints"),
-        filename="ddpm-{epoch:02d}-{loss:.2f}",
-        every_n_epochs=kwargs.get("chkpt_interval", 10),
+        filename="ddpm-{epoch:02d}-{loss:.4f}",
+        every_n_epochs=kwargs.get("chkpt_interval", 1),
         save_on_train_epoch_end=True,
     )
 
@@ -99,6 +112,7 @@ def train(root, **kwargs):
     train_kwargs["callbacks"] = [chkpt_callback]
 
     device = kwargs.get("device")
+    loader_kws = {}
     if device.startswith("gpu"):
         _, devs = configure_device(device)
         train_kwargs["gpus"] = devs
@@ -107,12 +121,24 @@ def train(root, **kwargs):
         from pytorch_lightning.plugins import DDPPlugin
 
         train_kwargs["plugins"] = DDPPlugin(find_unused_parameters=False)
+        loader_kws["persistent_workers"] = True
     elif device == "tpu":
         train_kwargs["tpu_cores"] = 8
 
     # Half precision training
     if kwargs.get("fp16"):
         train_kwargs["precision"] = 16
+
+    # Loader
+    loader = DataLoader(
+        dataset,
+        batch_size,
+        num_workers=kwargs.get("workers"),
+        pin_memory=True,
+        shuffle=True,
+        drop_last=True,
+        **loader_kws,
+    )
 
     logger.info(f"Running Trainer with kwargs: {train_kwargs}")
     trainer = pl.Trainer(**train_kwargs)
