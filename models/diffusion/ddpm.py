7ecc18c43dd9ce21bb3f9b3898a89a959e755ced
Kushagra Pandey <kpandeyce008@gmail.com>
2021-08-14 11:00:08 +0530

    Updated ddpm

--- models/diffusion/ddpm.py
+++ models/diffusion/ddpm.py
@@ -3,9 +3,15 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 
-from .unet import UNet
+from unet import Unet
 
 from tqdm import tqdm
+
+
+def extract(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
 
 
 class DDPM(pl.LightningModule):
@@ -18,49 +24,51 @@ class DDPM(pl.LightningModule):
         self.criterion = nn.L1Loss()
         self.lr = lr
 
-        # Precompute alpha_t for all t values
-        betas = np.linspace(self.beta_1, self.beta_2, self.T)
-        alphas = 1 - betas
-        alpha_bar = np.cumprod(alphas)
-        alpha_bar_shifted = np.append(1.0, alpha_bar[:-1])
-        sigma_t = betas * (1.0 - alpha_bar_shifted) / (1.0 - alpha_bar)
-
-        # Convert to torch tensors
-        self.betas = torch.from_numpy(betas)
-        self.alphas = torch.from_numpy(alphas)
-        self.alpha_bar = torch.from_numpy(alpha_bar)
-        self.alpha_bar_shifted = torch.from_numpy(alpha_bar_shifted)
-        self.sigma_t = torch.from_numpy(sigma_t)
-
         # Flag to keep track of device settings
-        self.const_dev_set = False
+        self.setup_consts = False
+
+    def setup_precomputed_const(self, dev):
+        # Main
+        self.betas = torch.linspace(self.beta_1, self.beta_2, self.T, device=dev)
+        self.alphas = 1 - self.betas
+        self.alpha_bar = torch.cumprod(self.alphas, dim=0)
+        self.alpha_bar_shifted = torch.cat([torch.tensor([1.0]), self.alpha_bar[:-1]])
+
+        # Posterior covariance of the forward process
+        self.post_variance = (
+            self.betas * (1.0 - self.alpha_bar_shifted) / (1.0 - self.alpha_bar)
+        )
+
+        # Auxillary consts
+        self.sqrt_alpha_bar = torch.sqrt(self.alpha_bar)
+        self.minus_sqrt_alpha_bar = torch.sqrt(1 - self.alpha_bar)
+        self.post_coeff_1 = (
+            torch.sqrt(self.alpha_bar_shifted) * self.betas / (1 - self.alpha_bar)
+        )
+        self.post_coeff_2 = (
+            torch.sqrt(self.alphas)
+            * (1 - self.alpha_bar_shifted)
+            / (1 - self.alpha_bar)
+        )
 
     def get_posterior_mean_covariance(self, x_t, t, clip_denoised=True):
         t_ = torch.full((x_t.size(0),), t, device=x_t.device, dtype=torch.long)
         # Generate the reconstruction from x_t
         x_recons = (
             x_t
-            - self.decoder(x_t, t_)
-            * torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None]
-        ) / torch.sqrt(self.alpha_bar[t])[:, None, None, None]
+            - self.decoder(x_t, t_) * extract(self.minus_sqrt_alpha_bar, t_, x_t.shape)
+        ) / extract(self.sqrt_alpha_bar, t_, x_t.shape)
 
         # Clip
         if clip_denoised:
             x_recons.clamp_(-1.0, 1.0)
 
         # Compute posterior mean from the reconstruction
-        a_1 = (
-            torch.sqrt(self.alpha_bar_shifted[t])
-            * self.betas[t]
-            / (1 - self.alpha_bar[t])
+        post_mean = (
+            extract(self.post_coeff_1, t_, x_t.shape) * x_recons
+            + extract(self.post_coeff_2, t_, x_t.shape) * x_t
         )
-        a_2 = (
-            torch.sqrt(self.alphas[t])
-            * (1 - self.alpha_bar_shifted[t])
-            / (1 - self.alpha_bar[t])
-        )
-        post_mean = a_1[:, None, None, None] * x_recons + a_2[:, None, None, None] * x_t
-        post_variance = self.sigma_t[t][:, None, None, None]
+        post_variance = extract(self.post_variance, t_, x_t.shape)
         return post_mean, post_variance
 
     def forward(self, x_t):
@@ -69,34 +77,30 @@ class DDPM(pl.LightningModule):
 
         # Set device
         dev = x_t.device
-        self.betas.to(dev)
-        self.alphas.to(dev)
-        self.alpha_bar.to(dev)
-        self.sigma_t.to(dev)
+        if not self.setup_consts:
+            self.setup_precomputed_const(dev)
+            self.setup_consts = True
 
         for t in tqdm(reversed(range(0, self.T))):
             z = torch.randn_like(x_t)
             post_mean, post_variance = self.get_posterior_mean_covariance(x, t)
+            # Langevin step!
             x = post_mean + torch.sqrt(post_variance) * z
         return x
 
-    def compute_noisy_input(self, x, eps, t):
+    def compute_noisy_input(self, x_start, eps, t):
+        assert eps.shape == x_start.shape
         # Samples the noisy input x_t ~ N(x_t|x_0) in the forward process
-        return (
-            x * torch.sqrt(self.alpha_bar[t])[:, None, None, None]
-            + eps * torch.sqrt(1 - self.alpha_bar[t])[:, None, None, None]
+        return x_start * extract(self.sqrt_alpha_bar, t, x_start.shape) + eps * extract(
+            self.minus_sqrt_alpha_bar, t, x_start.shape
         )
 
     def training_step(self, batch, batch_idx):
         x = batch
         dev = x.device
-        if not self.const_dev_set:
-            self.betas = self.betas.to(dev)
-            self.alphas = self.alphas.to(dev)
-            self.alpha_bar = self.alpha_bar.to(dev)
-            self.sigma_t = self.sigma_t.to(dev)
-
-            self.const_dev_set = True
+        if not self.setup_consts:
+            self.setup_precomputed_const(dev)
+            self.setup_consts = True
 
         # Sample timepoints
         t = torch.randint(0, self.T, size=(x.size(0),), device=dev)
@@ -106,7 +110,7 @@ class DDPM(pl.LightningModule):
 
         # Predict noise
         x_t = self.compute_noisy_input(x, eps, t)
-        eps_pred = self.decoder(x_t.float(), t)
+        eps_pred = self.decoder(x_t, t)
 
         # Compute loss
         loss = self.criterion(eps, eps_pred)
@@ -119,7 +123,7 @@ class DDPM(pl.LightningModule):
 
 
 if __name__ == "__main__":
-    decoder = UNet(t_embed_dim=64)
+    decoder = Unet(64)
     ddpm = DDPM(decoder)
     sample = torch.randn(4, 3, 128, 128)
     loss = ddpm.training_step(sample, 1)
