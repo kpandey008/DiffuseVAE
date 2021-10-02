9f80773ad9d902eb6ec9ec188f63069428256466
Kushagra Pandey <kpandeyce008@gmail.com>
2021-10-02 23:19:08 +0530

    Temporary script changes

--- scripts/test_ddpm.sh
+++ scripts/test_ddpm.sh
@@ -1,27 +1,44 @@
 ulimit -n 2048
-python test_ddpm.py sample-cond --n-steps 500 \
-                                --device gpu:0,1,2,3 \
-                                --save-path ~/ddpm_samples_celebahq_128_nsamples5k_form1/ \
-                                --num-samples 5000 \
-                                --compare False \
-                                --seed 0 \
-                                --batch-size 16 \
-                                --n-workers 8 \
-                                --checkpoints "" \
-                                ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
-                                ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt
 
-python test_ddpm.py sample-cond --n-steps 500 \
-                                --device gpu:0,1,2,3 \
-                                --save-path ~/ddpm_samples_celebahq_128_nsamples5k_form1_folder2/ \
-                                --num-samples 5000 \
-                                --compare False \
-                                --seed 1 \
-                                --batch-size 16 \
-                                --n-workers 8 \
-                                --checkpoints "" \
-                                ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
-                                ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt
+# CIFAR-10
+# python test_ddpm.py sample-cond --n-steps 700 \
+#                                 --device gpu:3 \
+#                                 --sample-prefix '3' \
+#                                 --seed 3 \
+#                                 --z-dim 512 \
+#                                 --image-size 32 \
+#                                 --save-path ~/ddpm_samples_cifar10_nsamples5k_form1/ \
+#                                 --num-samples 2500 \
+#                                 --compare False \
+#                                 --batch-size 64 \
+#                                 --n-workers 2 \
+#                                 --checkpoints "" \
+#                                 ~/checkpoints/cifar10/cifar10_ae/checkpoints/vae-epoch\=500-train_loss\=0.00.ckpt \
+#                                 ~/checkpoints/cifar10/ddpm_cifar10_form1/ddpmv2-epoch=970-loss=0.0889.ckpt
+
+# python test_ddpm.py sample-cond --n-steps 500 \
+#                                 --device gpu:0,1,2,3 \
+#                                 --save-path ~/ddpm_samples_celebahq_128_nsamples5k_form1/ \
+#                                 --num-samples 5000 \
+#                                 --compare False \
+#                                 --seed 0 \
+#                                 --batch-size 16 \
+#                                 --n-workers 8 \
+#                                 --checkpoints "" \
+#                                 ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
+#                                 ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt
+
+# python test_ddpm.py sample-cond --n-steps 500 \
+#                                 --device gpu:0,1,2,3 \
+#                                 --save-path ~/ddpm_samples_celebahq_128_nsamples5k_form1_folder2/ \
+#                                 --num-samples 5000 \
+#                                 --compare False \
+#                                 --seed 1 \
+#                                 --batch-size 16 \
+#                                 --n-workers 8 \
+#                                 --checkpoints "" \
+#                                 ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
+#                                 ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt
 
 # python test_ddpm.py generate-recons --n-steps 700 \
 #                                 --device gpu:0,1,2,3 \
--- scripts/test_uncond_ddpm.sh
+++ scripts/test_uncond_ddpm.sh
@@ -1,12 +1,25 @@
 ulimit -n 2048
-python test_ddpm.py sample --n-steps 300 \
+python test_ddpm.py sample --n-steps 1000 \
                             --device gpu:3 \
-                            --save-path ~/ddpm_samples_cifar10_nsamples10k_uncond/ \
                             --sample-prefix '3' \
-                            --num-samples 2500 \
                             --seed 3 \
                             --image-size 32 \
-                            --batch-size 32 \
+                            --save-path ~/ddpm_samples_cifar10_nsamples10k_uncond_fixedlarge_np/ \
+                            --num-samples 2500 \
+                            --batch-size 64 \
                             --n-workers 2 \
                             --checkpoints "" \
-                            ~/checkpoints/cifar10/ddpm_cifar10_uncond/ddpmv2-epoch\=1006-loss\=0.0662.ckpt
+                            --save-mode numpy \
+                            ~/checkpoints/cifar10/ddpm_cifar10_uncond/ddpmv2-epoch\=2031-loss\=0.0761.ckpt
+
+# python test_ddpm.py sample --n-steps 500 \
+#                             --device gpu:3 \
+#                             --sample-prefix '3' \
+#                             --seed 3 \
+#                             --save-path ~/ddpm_samples_celebahq128_nsamples5k_uncond/ \
+#                             --num-samples 1250 \
+#                             --image-size 128 \
+#                             --batch-size 16 \
+#                             --n-workers 3 \
+#                             --checkpoints "" \
+#                             ~/checkpoints/celebahq128/ddpm_celebahq128_uncond/ddpmv2-epoch\=795-loss\=0.0494.ckpt
