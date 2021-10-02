d137f702fd83b2165ce8be3e26a6b52095a259da
Kushagra Pandey <kpandeyce008@gmail.com>
2021-10-02 23:18:26 +0530

    Updated DDPM to use larger variance

--- models/diffusion/ddpm.py
+++ models/diffusion/ddpm.py
@@ -11,12 +11,15 @@ def extract(a, t, x_shape):
 
 
 class DDPM(nn.Module):
-    def __init__(self, decoder, beta_1=1e-4, beta_2=0.02, T=1000):
+    def __init__(
+        self, decoder, beta_1=1e-4, beta_2=0.02, T=1000, var_type="fixedlarge"
+    ):
         super().__init__()
         self.decoder = decoder
         self.T = T
         self.beta_1 = beta_1
         self.beta_2 = beta_2
+        self.var_type = var_type
 
         # Flag to keep track of device settings
         self.setup_consts = False
@@ -30,16 +33,54 @@ class DDPM(nn.Module):
             [torch.tensor([1.0], device=dev), self.alpha_bar[:-1]]
         )
 
-        # Posterior covariance of the forward process
-        self.post_variance = (
-            self.betas * (1.0 - self.alpha_bar_shifted) / (1.0 - self.alpha_bar)
+        assert self.alpha_bar_shifted.shape == torch.Size(
+            [
+                self.T,
+            ]
         )
 
         # Auxillary consts
         self.sqrt_alpha_bar = torch.sqrt(self.alpha_bar)
-        self.minus_sqrt_alpha_bar = torch.sqrt(1 - self.alpha_bar)
+        self.minus_sqrt_alpha_bar = torch.sqrt(1.0 - self.alpha_bar)
+        self.sqrt_recip_alphas_cumprod = torch.sqrt(1.0 / self.alpha_bar)
+        self.sqrt_recipm1_alphas_cumprod = torch.sqrt(1.0 / self.alpha_bar - 1)
+
+        # Posterior q(x_t-1|x_t,x_0,t) covariance of the forward process
+        self.post_variance = (
+            self.betas * (1.0 - self.alpha_bar_shifted) / (1.0 - self.alpha_bar)
+        )
+        # Clipping because post_variance is 0 before the chain starts
+        self.post_log_variance_clipped = torch.log(
+            torch.cat(
+                [
+                    torch.tensor([self.post_variance[1]], device=dev),
+                    self.post_variance[1:],
+                ]
+            )
+        )
+        self.p_variance, self.p_log_variance = {
+            # for fixedlarge, we set the initial (log-)variance like so
+            # to get a better decoder log likelihood.
+            "fixedlarge": (
+                self.betas,
+                torch.log(
+                    torch.cat(
+                        [
+                            torch.tensor([self.post_variance[1]], device=dev),
+                            self.betas[1:],
+                        ]
+                    )
+                ),
+            ),
+            "fixedsmall": (
+                self.post_variance,
+                self.post_log_variance_clipped,
+            ),
+        }[self.var_type]
+
+        # q(x_t-1 | x_t, x_0) mean coefficients
         self.post_coeff_1 = (
-            torch.sqrt(self.alpha_bar_shifted) * self.betas / (1 - self.alpha_bar)
+            self.betas * torch.sqrt(self.alpha_bar_shifted) / (1.0 - self.alpha_bar)
         )
         self.post_coeff_2 = (
             torch.sqrt(self.alphas)
@@ -47,14 +88,26 @@ class DDPM(nn.Module):
             / (1 - self.alpha_bar)
         )
 
+    def _predict_xstart_from_eps(self, x_t, t, eps):
+        assert x_t.shape == eps.shape
+        return (
+            extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t
+            - extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * eps
+        )
+
     def get_posterior_mean_covariance(self, x_t, t, clip_denoised=True, cond=None):
+        B = x_t.size(0)
         t_ = torch.full((x_t.size(0),), t, device=x_t.device, dtype=torch.long)
+        assert t_.shape == torch.Size(
+            [
+                B,
+            ]
+        )
+
         # Generate the reconstruction from x_t
-        x_recons = (
-            x_t
-            - self.decoder(x_t, t_, low_res=cond)
-            * extract(self.minus_sqrt_alpha_bar, t_, x_t.shape)
-        ) / extract(self.sqrt_alpha_bar, t_, x_t.shape)
+        x_recons = self._predict_xstart_from_eps(
+            x_t, t_, self.decoder(x_t, t_, low_res=cond)
+        )
 
         # Clip
         if clip_denoised:
@@ -65,15 +118,18 @@ class DDPM(nn.Module):
             extract(self.post_coeff_1, t_, x_t.shape) * x_recons
             + extract(self.post_coeff_2, t_, x_t.shape) * x_t
         )
-        post_variance = extract(self.post_variance, t_, x_t.shape)
-        return post_mean, post_variance
+
+        # Extract posterior variance
+        post_variance = extract(self.p_variance, t_, x_t.shape)
+        post_log_variance = extract(self.p_log_variance, t_, x_t.shape)
+        return post_mean, post_variance, post_log_variance
 
     def sample(self, x_t, cond=None, n_steps=None, checkpoints=[]):
         # The sampling process goes here!
         x = x_t
         sample_dict = {}
 
-        # Set device
+        # Set device and constants
         dev = x_t.device
         if not self.setup_consts:
             self.setup_precomputed_const(dev)
@@ -83,13 +139,23 @@ class DDPM(nn.Module):
         checkpoints = [num_steps] if checkpoints == [] else checkpoints
         for idx, t in enumerate(reversed(range(0, num_steps))):
             z = torch.randn_like(x_t)
-            post_mean, post_variance = self.get_posterior_mean_covariance(
+            (
+                post_mean,
+                post_variance,
+                post_log_variance,
+            ) = self.get_posterior_mean_covariance(
                 x,
                 t,
                 cond=cond,
             )
+            nonzero_mask = (
+                torch.tensor(t != 0, device=dev)
+                .float()
+                .view(-1, *([1] * (len(x_t.shape) - 1)))
+            )  # no noise when t == 0
+
             # Langevin step!
-            x = post_mean + torch.sqrt(post_variance) * z
+            x = post_mean + nonzero_mask * torch.exp(0.5 * post_log_variance) * z
 
             # Add results
             if idx + 1 in checkpoints:
@@ -98,7 +164,7 @@ class DDPM(nn.Module):
 
     def compute_noisy_input(self, x_start, eps, t):
         assert eps.shape == x_start.shape
-        # Samples the noisy input x_t ~ N(x_t|x_0) in the forward process
+        # Samples the noisy input x_t ~ q(x_t|x_0) in the forward process
         return x_start * extract(self.sqrt_alpha_bar, t, x_start.shape) + eps * extract(
             self.minus_sqrt_alpha_bar, t, x_start.shape
         )
