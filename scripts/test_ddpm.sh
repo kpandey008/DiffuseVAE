6926bd8ad5591d5d0452273ad9710bdb864c320f
Kushagra Pandey <kpandeyce008@gmail.com>
2021-10-29 13:56:56 +0530

    Added sample scripts

--- scripts/test_ddpm.sh
+++ scripts/test_ddpm.sh
@@ -1,53 +1,14 @@
-ulimit -n 2048
-
-# CIFAR-10
-# python test_ddpm.py sample-cond --n-steps 700 \
-#                                 --device gpu:3 \
-#                                 --sample-prefix '3' \
-#                                 --seed 3 \
-#                                 --z-dim 512 \
-#                                 --image-size 32 \
-#                                 --save-path ~/ddpm_samples_cifar10_nsamples5k_form1/ \
-#                                 --num-samples 2500 \
-#                                 --compare False \
-#                                 --batch-size 64 \
-#                                 --n-workers 2 \
-#                                 --checkpoints "" \
-#                                 ~/checkpoints/cifar10/cifar10_ae/checkpoints/vae-epoch\=500-train_loss\=0.00.ckpt \
-#                                 ~/checkpoints/cifar10/ddpm_cifar10_form1/ddpmv2-epoch=970-loss=0.0889.ckpt
-
-# python test_ddpm.py sample-cond --n-steps 500 \
-#                                 --device gpu:0,1,2,3 \
-#                                 --save-path ~/ddpm_samples_celebahq_128_nsamples5k_form1/ \
-#                                 --num-samples 5000 \
-#                                 --compare False \
-#                                 --seed 0 \
-#                                 --batch-size 16 \
-#                                 --n-workers 8 \
-#                                 --checkpoints "" \
-#                                 ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
-#                                 ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt
-
-# python test_ddpm.py sample-cond --n-steps 500 \
-#                                 --device gpu:0,1,2,3 \
-#                                 --save-path ~/ddpm_samples_celebahq_128_nsamples5k_form1_folder2/ \
-#                                 --num-samples 5000 \
-#                                 --compare False \
-#                                 --seed 1 \
-#                                 --batch-size 16 \
-#                                 --n-workers 8 \
-#                                 --checkpoints "" \
-#                                 ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
-#                                 ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt
-
-# python test_ddpm.py generate-recons --n-steps 700 \
-#                                 --device gpu:0,1,2,3 \
-#                                 --save-path ~/cond_inference_form1/ \
-#                                 --seed 0 \
-#                                 --num-samples 8 \
-#                                 --batch-size 2 \
-#                                 --compare True \
-#                                 --image-size 128 \
-#                                 ~/vaedm/checkpoints/vae-epoch\=189-train_loss\=0.00.ckpt \
-#                                 ~/ddpm_128_form1/ddpmv2-epoch\=801-loss\=0.0434.ckpt \
-#                                 ~/vaedm/reconstructions/
+python main/eval/ddpm/sample_cond.py +dataset=celebamaskhq128/test \
+                        dataset.ddpm.data.norm=False \
+                        dataset.ddpm.evaluation.seed=1234 \
+                        dataset.ddpm.evaluation.chkpt_path=\'/content/drive/MyDrive/ddpm_celebamaskhq_26thOct_form2_scale[01]/ddpmv2-celebamaskhq_26thOct_form2_scale01-epoch=250-loss=0.0022.ckpt\' \
+                        dataset.ddpm.evaluation.type='form2' \
+                        dataset.ddpm.evaluation.temp=0.1 \
+                        dataset.ddpm.evaluation.batch_size=4 \
+                        dataset.ddpm.evaluation.device=\'gpu:0\' \
+                        dataset.ddpm.evaluation.save_path=\'/content/ddpm_test_form2_temp=0.1\' \
+                        dataset.ddpm.evaluation.n_samples=32 \
+                        dataset.ddpm.evaluation.n_steps=200 \
+                        dataset.ddpm.evaluation.save_vae=True \
+                        dataset.ddpm.evaluation.workers=1 \
+                        dataset.vae.evaluation.chkpt_path=\'/content/drive/MyDrive/project_vaedm/celebahq-mask/vae_chkpt_celebahq/vae-epoch=189-train_loss=0.00.ckpt\'
--- scripts/train_ae.sh
+++ scripts/train_ae.sh
@@ -3,8 +3,10 @@ python main/train_ae.py +dataset=celebamaskhq128/train \
                      dataset.vae.data.root='/data/kushagrap20/datasets/CelebAMask-HQ/' \
                      dataset.vae.data.name='celebamaskhq' \
                      dataset.vae.training.batch_size=32 \
+                     dataset.vae.training.epochs=1500 \
                      dataset.vae.training.device=\'gpu:0,1,2,3\' \
                      dataset.vae.training.results_dir=\'/data/kushagrap20/vae_alpha_0_1/\' \
+                     dataset.vae.training.restore_path=\'/data/kushagrap20/vae_alpha_0_1/checkpoints/vae-celebamaskhq_alpha_0_1-epoch=999-train_loss=0.0000.ckpt\' \
                      dataset.vae.training.workers=4 \
                      dataset.vae.training.chkpt_prefix='celebamaskhq_alpha_0_1' \
                      dataset.vae.training.alpha=0.1
