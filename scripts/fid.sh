1523f234e66611ed3ff6c21fa022d8855e53eb09
Kushagra Pandey <kpandeyce008@gmail.com>
2022-09-02 10:22:18 +0530

    Script updates

--- scripts/fid.sh
+++ scripts/fid.sh
@@ -1,4 +1,4 @@
 # fidelity --gpu 3 --fid --input1 ~/datasets/CelebAMask-HQ-128/ --input2 ~/ddpm_cmhq_confirm_form2/49/images/ --input1-cache-name cmhq128
-fidelity --gpu 3 --isc --input1 ~/ddpm_cifar10_form1_repro_try1/2850/1000/images/ --input2 cifar10-train
+fidelity --gpu 3 --fid --input1 ~/ddpm_cifar10_form2_repro_try1/1000/1000/images/ --input2 cifar10-train
 # fidelity --gpu 3 --fid --input1 ~/datasets/img_align_celeba_64/ --input2 ~/ddpm_celeba64_benchmark_speedvsquality/form2_ddim/310/1000/images/ --input1-cache-name celeba64
 # fidelity --gpu 3 --fid --input1 ~/datasets/celeba_hq_256/ --input2 ~/ddpm_chq256_benchmark_speedvsquality/form1_ddim_expde/96/images/ --input1-cache-name chq256
